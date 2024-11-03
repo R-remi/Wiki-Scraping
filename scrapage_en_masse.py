0da21af1591740b02c1a67ef768e705fef28961b
Rémi <133678195+R-remi@users.noreply.github.com>
2024-11-03 23:29:30 +0100

    Update scrapage_en_masse.py

--- scrapage_en_masse.py
+++ scrapage_en_masse.py
@@ -1,25 +1,32 @@
+# Importer les bibliothèques nécessaires
 import requests
 from bs4 import BeautifulSoup
 import json
 import os
 
-# lire premiere de stack_de_wish.json
+# Fonction pour lire la premiere valeur du fichier stack_de_wish.json
 def read_json():
     with open('stack_de_wish.json', 'r', encoding='utf-8') as f:
         return json.load(f)
 
-# concater les liens entre url et stack_de_wish.json
+# Concater les liens entre url et stack_de_wish.json
 url = 'https://fr.wikipedia.org'
 def concat_links():
     json_data = read_json()
+    # Vérifie que la liste JSON n'est pas vide avant de lire le premier élément
     if json_data:
-        concatenated_link = url + json_data[0]
+        concatenated_link = url + json_data[0] if json_data[0].startswith('/') else url + '/' + json_data[0]
         return [concatenated_link]
     else:
         print("La liste JSON est vide.")
         return []
 
 def get_html(url):
+    if url in check_already_visited():
+        print(f"Le lien {url} a déjà été visité.")
+        erase_first()
+        return None
+
     response = requests.get(url)
     if response.status_code == 200:
         print(f"Requête réussie pour {url}")
@@ -58,23 +65,36 @@ def erase_first():
     else:
         print("La liste JSON est vide.")
 
-def main():
-    # Récupère le contenu HTML
+def fetch_html_content():
+    # Récupère le contenu HTML de la première page
     links = concat_links()
     if links:
-        html = get_html(links[0])
-    else:
-        html = None
+        return get_html(links[0])
+    return None
 
+def extract_links(html):
+    # Analyse le contenu HTML
+    soup = BeautifulSoup(html, 'html.parser')
+    # Trouve tous les éléments <a> et récupère les attributs 'href'
+    liens = [a['href'] for a in soup.find_all('a', href=True)]
+    # on suprimme les liens qui ne sont pas des articles wikipedia
+    return [lien for lien in liens if lien.startswith('/wiki/')]
+
+def check_already_visited():
+    file_path = 'stack_de_wish_visited.json'
+    if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            json_data = json.load(f)
+            return json_data
+    else:
+        return []
+
+def main():
+    html = fetch_html_content()
     if html:
-        # Analyse le contenu HTML
-        soup = BeautifulSoup(html, 'html.parser')
-        # Trouve tous les éléments <a> et récupère les attributs 'href'
-        liens = [a['href'] for a in soup.find_all('a', href=True)]
-        # on suprimme les liens qui ne sont pas des articles wikipedia
-        liens = [lien for lien in liens if lien.startswith('/wiki/')]
+        liens = extract_links(html)
         # on stocke les liens dans un fichier json
         add_to_json(liens)
 
-for i in range(25):
+for i in range(10000):
     main()
